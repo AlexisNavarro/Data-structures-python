379276fee88895b035bb740209278b4c31ded164
Alexis Navarro <navarroalexis007@gmail.com>
2023-04-12 18:13:50 -0600

    implemented the add functionality to the array list, need to implement remove

---
+++ arrays.py
@@ -0,0 +1,80 @@
+def add(names):
+    print("\nin the add method")
+
+    position = int(input("Select what index you want to add a name to "))
+    name = input("Select what name you want to add ")
+
+    size = len(names) + 1
+    new_arr = []*size 
+    holder = ""
+
+    count = 0
+
+    #add at the beginning of the list
+    if position == 0:      
+        for i in range(1):
+            new_arr.append(name)
+        for items in names:
+            new_arr.append(items)
+        print(new_arr)
+    
+    #add anywhere at the list based on the index. Avoided using the insert function.
+    else:
+        for i in range(size):
+            if i != position:
+                new_arr.append(names[count])
+                count+=1
+            else:
+                new_arr.append(name)
+            print(new_arr)
+        
+
+
+
+
+def remove(names):
+    print("in the remove method")
+
+
+
+def single_array():
+    print("\ninside single array")
+
+    names = ["John","Alexis","Jose"]
+
+    loop_type = input("1.in range loop\n2. i in names loop ")
+
+    if int(loop_type) == 1:
+        for i in range(len(names)):
+            print(names[i])
+    elif int(loop_type) == 2:
+        for i in names:
+            print(i)
+
+    user_in = input("1. add elements to the array\n2. remove elements from the array ")
+
+    match int(user_in):
+        case 1:
+            add(names)
+        case 2:
+            remove(names)
+
+
+
+def double_array():
+    print("inside double array")
+
+def main():
+    user_in = input("1. for singly array list\n2. for doubly array list ")
+
+    match int(user_in):
+        case 1:
+            single_array()
+        case 2:
+            double_array()
+
+
+
+
+if __name__ == '__main__':
+    main()
