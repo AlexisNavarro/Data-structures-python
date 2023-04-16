b9a685a032edab7199feea66d29c93c8b97d1f74
Alexis Navarro <navarroalexis007@gmail.com>
2023-04-16 02:35:42 -0600

    implemented a priority queue while using a dictionary

--- Queues/queue1.py
+++ Queues/queue1.py
@@ -2,10 +2,12 @@
 
 from collections import deque
 from queue import Queue
+import queue
+
 def reg_queue():
     print("accessed regular queue method")
 
-    user_in= int(input("1. create a queue from deque import \n2. create a queue from queue import "))
+    user_in= int(input("1. create a queue from deque import \n2. create a queue from Queue import "))
 
     if user_in == 1:
         q = deque()
@@ -50,6 +52,33 @@ def reg_queue():
 
 def prio_queue():
     print("accessed priority queue method")
+    user_in= int(input("1. create a priority queue from queue import \n2. create a priority queue using a list "))
+    if user_in == 1:
+        #using the queue import to access the priority queue functions
+        pq = queue.PriorityQueue()
+
+        pq.put((4, "Rathalos"))
+        pq.put((3, "Lacriacus"))
+        pq.put((5, "Rathian"))
+
+        print(pq.get())
+    elif user_in == 2:
+
+        priority_keys = [1,2,3,4,5]
+        
+        monsters = {3: "Lacriacus", 1: "Lagombi", 4: "Rathalos", 5: "Rathian"}
+        monster_items = monsters.items()
+
+        print(sorted(monster_items))
+
+        for i in range(len(priority_keys)):
+            try:
+                print(monsters.pop(priority_keys[i]))
+            except KeyError:
+                print("no key found")
+
+
+        
 
 
 def main():
