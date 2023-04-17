e1396526746d232c3bec0698995dc8f4cdc35581
Alexis Navarro <navarroalexis007@gmail.com>
2023-04-17 00:29:42 -0600

    implemented in order, pre order, and post order traversal methods

--- Binary_trees/binary_tree1.py
+++ Binary_trees/binary_tree1.py
@@ -3,7 +3,7 @@ class Node:
         self.left = None
         self.right = None
         self.data = data
-    ''''''
+
     def insert(self, data):
         if self.data is not None:
 
@@ -22,6 +22,40 @@ class Node:
         else:
             self.data = data
     
+
+    #method to traverse the left subtree then the root and then the right subtree
+    def in_order_traversal(self, root):
+        in_order = []
+
+        if root is not None:
+            in_order = self.in_order_traversal(root.left) #first traverse every left node and then append the value to the list
+            in_order.append(root.data)
+            in_order += self.in_order_traversal(root.right)#traverse every node on the right side and append it to the list
+        return in_order
+    
+
+    #method to traverse the root, then the left subtree and then the right subtree
+    def pre_order_traversal(self,root):
+        pre_order = []
+
+        if root is not None:
+            pre_order.append(root.data) #append the root node
+            pre_order += self.pre_order_traversal(root.left) #add every node value to the left side of the tree from top to bottom 
+            pre_order += self.pre_order_traversal(root.right) #add every node value to the right side of the tree from top to bottom 
+        return pre_order
+
+
+    #method to traverse the left subtree, then the right subtree, then the root
+    def post_order_traversal(self, root):
+        post_order = []
+
+        if root is not None:
+            post_order = self.post_order_traversal(root.left) #traverse every left subtree node and add it to the list
+            post_order += self.post_order_traversal(root.right)#traverse every right subtree node and add it to the list
+            post_order.append(root.data)
+        return post_order
+    
+
     def print_binary_tree(self):
         if self.left:
             self.left.print_binary_tree()
@@ -42,6 +76,10 @@ def main ():
     root.insert(20)
     root.insert(21)
     root.print_binary_tree()
+
+    print(root.in_order_traversal(root))
+    print(root.pre_order_traversal(root))
+    print(root.post_order_traversal(root))
 
 if __name__ == '__main__':
     main()
